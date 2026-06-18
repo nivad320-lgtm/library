7966dc29c9ae0c93c74d6ffc665560bcf4b11f9c
Davin <286156958+nivad320-lgtm@users.noreply.github.com>
2026-06-18 15:31:30 +0800

    Add dialog feature

--- javascript.js
+++ javascript.js
@@ -15,5 +15,50 @@ function addBookToLibrary(name, author, pages, read) {
 }
 
 addBookToLibrary('steve', 'walter', 333, true);
+addBookToLibrary('Diary', 'walter', 333, true);
 console.table(myLibrary);
 
+
+const container = document.querySelector('.container');
+const bookList = document.createElement('div');
+container.appendChild(bookList);
+
+for(book of myLibrary) {
+  const bookDescription = document.createElement('p');
+  bookDescription.textContent = `${book.name} by ${book.author}. ${book.pages}pages. Status: ${book.read}`;
+  bookList.appendChild(bookDescription);
+}
+
+
+const showButton = document.getElementById("showDialog");
+const favDialog = document.getElementById("favDialog");
+const outputBox = document.querySelector("output");
+const selectEl = favDialog.querySelector("select");
+const bookAuthor = favDialog.querySelector("#author");
+const bookPages = favDialog.querySelector("#pages");
+const bookTitle = favDialog.querySelector("#title");
+const confirmBtn = favDialog.querySelector("#confirmBtn");
+
+showButton.addEventListener('click', () => {
+  favDialog.showModal();
+});
+
+favDialog.addEventListener('close', (e) => {
+  outputBox.value = 
+    favDialog.returnValue === "default" ? "No return value."
+    : `ReturnValue: ${favDialog.returnValue}.`;
+})
+
+confirmBtn.addEventListener("click", (event) => {
+  event.preventDefault(); // We don't want to submit this fake form
+  favDialog.close(selectEl.value); // Have to send the select box value here.
+  favDialog.close(bookAuthor.value);
+  favDialog.close(bookPages.value);
+  favDialog.close(bookTitle.value);
+  
+  const bookDescription = document.createElement('p');
+  bookDescription.textContent = `${bookTitle.value} by ${bookAuthor.value}. ${bookPages.value}pages. Status: ${selectEl.value}`;
+  bookList.appendChild(bookDescription);
+
+
+});
